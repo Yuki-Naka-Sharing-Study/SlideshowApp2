6636b80a53e7f6729c2e96b1e88fecfd382e0bb2
Yuki Naka <yu-ki216m@outlook.jp>
2023-01-13 19:57:27 +0900

    Cannot assign value of type 'UIImage' to type 'UIImageView'

--- TestSlideshowApp/ViewController.swift
+++ TestSlideshowApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 画像を設定
+        let sampleImage = UIImage(named: "winter.jpeg")
+        image.image = sampleImage
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextVC = storyboard.instantiateViewController(withIdentifier: "NestViewController") as! ViewController
+
+        //ViewControllerのimage(画像データ)をNextViewControllerのimage(変数)に渡す
+        nextVC.image = image.image!
+
+        //画面遷移
+        present(nextVC, animated: true, completion: nil)
     }
-
-
+    
+    @IBAction func imageViewTapped(_ sender: Any) {
+        print("タップ")
+    }
 }
 
