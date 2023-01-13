49cc5903b25196e5b35bc3b2cdd37d7edc9d90fa
Yuki Naka <yu-ki216m@outlook.jp>
2023-01-13 15:04:49 +0900

    Initial Commit

---
+++ TestSlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSlideshowApp
+//
+//  Created by 仲優樹 on 2023/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
