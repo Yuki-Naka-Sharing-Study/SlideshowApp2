b409871c28fe3bc71e12c300929f64c7cac695e3
Yuki Naka <yu-ki216m@outlook.jp>
2023-01-13 15:21:28 +0900

    一旦「UI」のみ完成

---
+++ TestSlideshowApp/NextViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NextViewController.swift
+//  TestSlideshowApp
+//
+//  Created by 仲優樹 on 2023/01/13.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
