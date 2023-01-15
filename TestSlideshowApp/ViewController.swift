855e4b43e117aca880d24b505989e9cfffd6069d
Yuki Naka <yu-ki216m@outlook.jp>
2023-01-15 16:07:28 +0900

    再生、一時停止、リセット機能を付ける（仮）

--- TestSlideshowApp/ViewController.swift
+++ TestSlideshowApp/ViewController.swift
@@ -31,5 +31,45 @@ class ViewController: UIViewController {
         //画面遷移
         present(nextVC, animated: true, completion: nil)
     }
+    
+    @IBOutlet weak var timerLabel: UILabel!
+
+       // タイマー
+       var timer: Timer!
+
+       // タイマー用の時間のための変数
+       var timer_sec: Float = 0
+    
+        @objc func updateTimer(_ timer: Timer) {
+            self.timer_sec += 0.1
+            self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+        }
+
+        // 再生ボタン IBAction
+        @IBAction func startTimer(_ sender: Any) {
+            // 動作中のタイマーを1つに保つために、 timer が存在しない場合だけ、タイマーを生成して動作させる
+            if self.timer == nil {
+                self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+            }
+        }
+
+        // 一時停止ボタン IBAction
+        @IBAction func pauseTimer(_ sender: Any) {
+            // タイマーを停止
+            self.timer.invalidate()
+            self.timer = nil          // startTimer() の self.timer == nil で判断するために、 self.timer = nil としておく
+        }
+    
+        // リセットボタン IBAction
+        @IBAction func resetTimer(_ sender: Any) {
+            // リセットボタンを押すと、タイマーの時間を0に
+            self.timer_sec = 0
+            self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+            
+            if self.timer != nil {
+                self.timer.invalidate()   // タイマーを停止する
+                self.timer = nil          // startTimer() の self.timer == nil で判断するために、 self.timer = nil としておく
+            }
+        }
 }
 
