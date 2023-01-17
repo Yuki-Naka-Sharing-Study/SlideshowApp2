d327ffda132396ccacc37ad8b262c3f53a6420c2
Yuki Naka <yu-ki216m@outlook.jp>
2023-01-17 15:50:45 +0900

    【課題進捗】①進むボタンで1つ先の画像を表示し、戻るボタンで1つ前の画像を表示します 　②最後の画像の表示時に、進むボタンをタップすると、最初の画像が表示されるようにしてください 　③最初の画像の表示時に、戻るボタンをタップすると、最後の画像が表示されるようにしてください

--- TestSlideshowApp/ViewController.swift
+++ TestSlideshowApp/ViewController.swift
@@ -11,6 +11,62 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBAction func Back(sender: AnyObject) {
+
+      // 表示している画像の番号を1減らす
+      dispImageNo -= 1
+
+      // 表示している画像の番号を元に画像を表示する
+      displayImage()
+        
+    }
+
+    @IBAction func Go(sender: AnyObject) {
+
+      // 表示している画像の番号を1増やす
+      dispImageNo += 1
+
+      // 表示している画像の番号を元に画像を表示する
+      displayImage()
+        
+    }
+
+    /// 表示している画像の番号
+    var dispImageNo = 0
+
+    /// 表示している画像の番号を元に画像を表示する
+    func displayImage() {
+
+        // 画像の名前の配列
+        let imageNameArray = [
+            "winter.jpeg",
+            "spring.jpeg",
+            "summer.jpeg",
+        ]
+        
+        // 画像の番号が正常な範囲を指しているかチェック
+
+        // 範囲より下を指している場合、最後の画像を表示
+        if dispImageNo < 0 {
+            dispImageNo = 2
+        }
+
+        // 範囲より上を指している場合、最初の画像を表示
+        if dispImageNo > 2 {
+            dispImageNo = 0
+        }
+        
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[dispImageNo]
+
+        // 画像を読み込み
+        let image = UIImage(named: name)
+
+        // Image Viewに読み込んだ画像をセット
+        imageView.image = image
+            
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // 画像を設定
@@ -32,7 +88,7 @@ class ViewController: UIViewController {
     }
     
     // outletの接続
-    @IBOutlet weak var playbackStopButton: UIButton!
+        @IBOutlet weak var playbackStopButton: UIButton!
     
         // 配列に指定するindex番号を宣言
         var nowIndex:Int = 0
